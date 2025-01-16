8d983341da7aff1368e234d91fa4760bd949c6b9
Shared-Westover <Shared.Westover@2k.com>
2025-01-16 10:53:42 -0600

    new file structure and cleanup

--- depth_snaps.sql
+++
@@ -1,9 +0,0 @@
-with ddepth as (select d.*, s.pfr_player_id from depth d join full_ids s
-on d.gsis_id = s.gsis_id 
-)
-select * from ddepth dd
-join snaps s ON
-dd.pfr_player_id = s.pfr_player_id
-and dd.season = s.season
-and dd.week = s.week
-;
