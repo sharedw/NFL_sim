fabe3456c6babb7742002d2b0512cf40263db7de
Shared-Westover <Shared.Westover@2k.com>
2025-10-07 13:13:40 -0500

    import cleanup

--- src/utils/quack.py
+++ src/utils/quack.py
@@ -4,7 +4,7 @@ import duckdb
 class Quack:
 
     @staticmethod
-    def fetch_table(table_name: str, db_path: str = "C:/Github/nfl_sim/data/nfl.duckdb"):
+    def fetch_table(table_name: str, db_path: str = "data/nfl.duckdb"):
         con = duckdb.connect(db_path)
         try:
             df = con.execute(f"SELECT * FROM {table_name}").fetch_df()
@@ -14,7 +14,7 @@ class Quack:
 
     @staticmethod
     def query(query):
-        con = duckdb.connect("C:/Github/nfl_sim/data/nfl.duckdb")
+        con = duckdb.connect("data/nfl.duckdb")
         try:
             df = con.execute(query).fetch_df()
         finally:
@@ -23,7 +23,7 @@ class Quack:
     
     @staticmethod
     def file_query(query_file_name):
-        con = duckdb.connect("C:/Github/nfl_sim/data/nfl.duckdb")
+        con = duckdb.connect("data/nfl.duckdb")
         try:
             with open(f"./data/queries/{query_file_name}.sql", "r") as f:
                 sql = f.read()
