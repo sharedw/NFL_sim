d68e8c1f0de5c3d10ea037f53e305cb62b652082
sharedw <sharedwestover@gmail.com>
2025-10-22 08:30:47 -0500

    Update sim.py

--- src/sim_utils/sim.py
+++ src/sim_utils/sim.py
@@ -1,9 +1,10 @@
 from sim_utils.game import GameState
 from sim_utils.team import Team
-import pandas as pd
+from utils.quack import Quack
 
-
-def sim_games(team1_name: str, team2_name: str, season:int, week:int, n_games:int=100) -> pd.DataFrame:
+def sim_games(team1_name: str, team2_name: str, season:int, week:int, n_games:int=100) -> list:
+	result = []
+	
 	t1 = Team(team1_name, season, week)
 	t2 = Team(team2_name, season, week)
 
@@ -21,3 +22,10 @@ def sim_games(team1_name: str, team2_name: str, season:int, week:int, n_games:in
 	print(df)
 	return result
 
+def get_actual_results(team1_name: str, team2_name: str, season:int, week:int) -> pd.DataFrame:
+	Quack.query("""
+	select * from weekly where season = {season}
+			 and week = {week}
+			 and ((home = {team1_name} and away = {team2_name})
+			 or 
+			 (home = {team2_name} and away = {team1_name}))""")
